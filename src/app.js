59f7652163a85b39465653186981c3c8f01b71a9
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2021-11-20 15:48:33 -0500

    Added js to show current temp

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function showCurrentTemperature(response) {
+  console.log(response.data);
+  let city = document.querySelector("h1");
+  city.innerHTML = response.data.name;
+  let currentTemperature = document.querySelector("#current-temp");
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].main;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "42d967004f943c4e9a88d5763e34cc28";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showCurrentTemperature);
