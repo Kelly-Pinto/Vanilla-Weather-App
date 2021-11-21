5a9f7b65bf1363faf0d1212fe6c27b27a9337d83
Kelly-Pinto <91966302+Kelly-Pinto@users.noreply.github.com>
2021-11-20 20:14:32 -0500

    Updated current time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showCurrentTemperature(response) {
-  console.log(response.data);
   let city = document.querySelector("h1");
   city.innerHTML = response.data.name;
   let currentTemperature = document.querySelector("#current-temp");
@@ -10,9 +32,12 @@ function showCurrentTemperature(response) {
   humidity.innerHTML = response.data.main.humidity;
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+  let date = document.querySelector("#current-day-time");
+  date.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "42d967004f943c4e9a88d5763e34cc28";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showCurrentTemperature);
